ca5913482aeeaf6b0c079e0fd9741c3b712659e1
Joachim Van der Auwera <joachim@geosparc.com>
2012-12-03 21:47:48 +0100

    fixes to prepare for 0.1 release (compile for Java 5, remove faulty geomajas references)

--- core/src/test/java/org/jtransfo/ToConverterTest.java
+++ core/src/test/java/org/jtransfo/ToConverterTest.java
@@ -2,10 +2,8 @@
  * This file is part of jTransfo, a library for converting to and from transfer objects.
  * Copyright (c) PROGS bvba, Belgium
  *
- * The program is available in open source according to the Apache
- * License, Version 2.0. All contributions in this program are covered
- * by the Geomajas Contributors License Agreement. For full licensing
- * details, see LICENSE.txt in the project root.
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
  */
 
 package org.jtransfo;
@@ -16,9 +14,6 @@ import org.junit.rules.ExpectedException;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-/**
- * Test for {@link ToConverter}.
- */
 public class ToConverterTest {
 
     @Rule
--- core/src/test/java/org/jtransfo/object/SimpleExtendedTo.java
+++ core/src/test/java/org/jtransfo/object/SimpleExtendedTo.java
@@ -2,10 +2,8 @@
  * This file is part of jTransfo, a library for converting to and from transfer objects.
  * Copyright (c) PROGS bvba, Belgium
  *
- * The program is available in open source according to the Apache
- * License, Version 2.0. All contributions in this program are covered
- * by the Geomajas Contributors License Agreement. For full licensing
- * details, see LICENSE.txt in the project root.
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
  */
 
 package org.jtransfo.object;
@@ -14,9 +12,6 @@ import org.jtransfo.DomainClass;
 import org.jtransfo.MappedBy;
 import org.jtransfo.NotMapped;
 
-/**
- * TO with base class.
- */
 @DomainClass(domainClass = SimpleExtendedDomain.class)
 public class SimpleExtendedTo extends SimpleBaseTo {
 
