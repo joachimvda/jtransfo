b1b4633d1b6a34df0a0b7d07c8cbca2788771ff9
Joachim Van der Auwera <joachim@progs.be>
2012-12-18 08:07:45 +0100

    TR-8 string-enum type converter

---
+++ core/src/main/java/org/jtransfo/StringEnumTypeConverter.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of jTransfo, a library for converting to and from transfer objects.
+ * Copyright (c) PROGS bvba, Belgium
+ *
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
+ */
+
+package org.jtransfo;
+
+import org.jtransfo.JTransfoException;
+import org.jtransfo.TypeConverter;
+
+/**
+ * Type converter for representing enums as string in the transfer object.
+ *
+ * @param <ENUM_TYPE> enum type to convert with
+ */
+public class StringEnumTypeConverter<ENUM_TYPE extends Enum> implements TypeConverter<String, ENUM_TYPE> {
+
+    private final Class<ENUM_TYPE> enumClass;
+
+    /**
+     * Construct a new type converter for the given enum type.
+     *
+     * @param enumClass enum type class
+     */
+    public StringEnumTypeConverter(Class<ENUM_TYPE> enumClass) {
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public boolean canConvert(Class<?> realToType, Class<?> realDomainType) {
+        return String.class.isAssignableFrom(realToType) && enumClass.isAssignableFrom(realDomainType);
+    }
+
+    @Override
+    public ENUM_TYPE convert(String object) throws JTransfoException {
+        if (null == object || 0 == object.length()) {
+            return null;
+        }
+
+        return (ENUM_TYPE) Enum.valueOf(enumClass, object);
+    }
+
+    @Override
+    public String reverse(ENUM_TYPE object) throws JTransfoException {
+        if (null == object) {
+            return null;
+        }
+        return object.name();
+    }
+}
---
+++ core/src/test/java/org/jtransfo/StringEnumTypeConverterTest.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of jTransfo, a library for converting to and from transfer objects.
+ * Copyright (c) PROGS bvba, Belgium
+ *
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
+ */
+
+package org.jtransfo;
+
+import org.jtransfo.object.Gender;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Test for StringEnumTypeConverterTest.
+ */
+public class StringEnumTypeConverterTest {
+
+    private TypeConverter<String, Gender> enumTypeConverter;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setup() throws Exception {
+        enumTypeConverter = new StringEnumTypeConverter(Gender.class);
+    }
+
+    @Test
+    public void testCanConvert() throws Exception {
+        assertThat(enumTypeConverter.canConvert(String.class, Gender.class)).isTrue();
+        assertThat(enumTypeConverter.canConvert(Object.class, Gender.class)).isFalse();
+        assertThat(enumTypeConverter.canConvert(String.class, String.class)).isFalse();
+        assertThat(enumTypeConverter.canConvert(Gender.class, String.class)).isFalse();
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+        assertThat(enumTypeConverter.convert(Gender.MALE.name())).isEqualTo(Gender.MALE);
+        assertThat(enumTypeConverter.convert(Gender.FEMALE.name())).isEqualTo(Gender.FEMALE);
+        assertThat(enumTypeConverter.convert(null)).isNull();
+
+        exception.expect(IllegalArgumentException.class);
+        assertThat(enumTypeConverter.convert("blabla"));
+    }
+
+    @Test
+    public void testReverse() throws Exception {
+        assertThat(enumTypeConverter.reverse(Gender.MALE)).isEqualTo(Gender.MALE.name());
+        assertThat(enumTypeConverter.reverse(Gender.FEMALE)).isEqualTo(Gender.FEMALE.name());
+        assertThat(enumTypeConverter.reverse(null)).isNull();
+    }
+
+}
