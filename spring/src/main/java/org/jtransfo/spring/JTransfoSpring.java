5b50a9d8a49c0cebcd203b85200d8e6fffacd3bc
joachimvda <joachim@progs.be>
2013-01-04 21:19:23 +0100

    TR-3 checkstyle fix

--- spring/src/main/java/org/jtransfo/spring/JTransfoSpring.java
+++ spring/src/main/java/org/jtransfo/spring/JTransfoSpring.java
@@ -1,3 +1,11 @@
+/*
+ * This file is part of jTransfo, a library for converting to and from transfer objects.
+ * Copyright (c) PROGS bvba, Belgium
+ *
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
+ */
+
 package org.jtransfo.spring;
 
 import org.jtransfo.JTransfoImpl;
@@ -19,6 +27,9 @@ public class JTransfoSpring extends JTransfoImpl {
     @Autowired(required = false)
     private List<TypeConverter> typeConverters;
 
+    /**
+     * Get object finders and type converters from Spring configuration.
+     */
     @PostConstruct
     protected void postConstruct() {
         if (null != typeConverters) {
