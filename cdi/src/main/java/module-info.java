a5ef5fb0aaa5bbb400503b2bdc19603a2015f16c
joachimvda <joachim@progs.be>
2017-11-10 10:11:19 +0100

    #63 java9

---
+++ cdi/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * This file is part of jTransfo, a library for converting to and from transfer objects.
+ * Copyright (c) PROGS bvba, Belgium
+ *
+ * The program is available in open source according to the Apache License, Version 2.0.
+ * For full licensing details, see LICENSE.txt in the project root.
+ */
+
+module org.jtransfo.cdi {
+    exports org.jtransfo.cdi;
+    requires org.jtransfo;
+}
